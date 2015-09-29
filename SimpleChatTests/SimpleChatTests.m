d25f30c2ecf87a8652f917ef6824dcbc556f1ee3
Anton Pomozov <pomozov@shtrih-m.ru>
2015-09-29 12:48:28 +0300

    Initial Commit

---
+++ SimpleChatTests/SimpleChatTests.m
@@ -0,0 +1,39 @@
+//
+//  SimpleChatTests.m
+//  SimpleChatTests
+//
+//  Created by Anton Pomozov on 29.09.15.
+//  Copyright © 2015 Akademon Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimpleChatTests : XCTestCase
+
+@end
+
+@implementation SimpleChatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
