a4e9b4a58f34f7c54eeaf1caed73e32804cacd80
Anton Pomozov <pomozoff@gmail.com>
2015-10-05 23:45:35 +0300

    [Add]: Collection manager

---
+++ SimpleChat/Models/Managers/CollectionManager.m
@@ -0,0 +1,13 @@
+//
+//  CollectionManager.m
+//  SimpleChat
+//
+//  Created by Антон on 05.10.15.
+//  Copyright © 2015 Akademon Ltd. All rights reserved.
+//
+
+#import "CollectionManager.h"
+
+@implementation CollectionManager
+
+@end
