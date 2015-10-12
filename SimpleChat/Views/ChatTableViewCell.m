37f6188786af6bc866feb334705ca987b9f34f76
Anton Pomozov <pomozov@shtrih-m.ru>
2015-10-12 20:16:40 +0300

    [Add]: Drag received image

--- SimpleChat/Views/ChatTableViewCell.m
+++ SimpleChat/Views/ChatTableViewCell.m
@@ -10,7 +10,7 @@
 
 #import "ChatTableViewCell.h"
 
-@interface ChatTableViewCell ()
+@interface ChatTableViewCell () <UIGestureRecognizerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIView *bubbleView;
 @property (weak, nonatomic) IBOutlet UILabel *messageTextLabel;
@@ -26,6 +26,11 @@
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *gapBetweenMapAndTextConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *mapHeightConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *mapAspectRatioConstraint;
+
+@property (nonatomic, assign) BOOL isImageTapped;
+@property (nonatomic, strong) UILongPressGestureRecognizer *longPressGestureRecognizer;
+@property (nonatomic, strong) UIPanGestureRecognizer *panGestureRecognizer;
+@property (nonatomic, strong) UIImageView *panningImageView;
 
 @end
 
@@ -60,6 +65,23 @@ static UILayoutPriority const kMinConstraintPriority = 200;
 - (void)setHasTail:(BOOL)hasTail {
     _hasTail = hasTail;
     self.bubbleTail.hidden = !hasTail;
+}
+
+- (UILongPressGestureRecognizer *)longPressGestureRecognizer {
+    if (!_longPressGestureRecognizer) {
+        _longPressGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self
+                                                                                    action:@selector(didLongTapOnImage:)];
+        _longPressGestureRecognizer.delegate = self;
+    }
+    return _longPressGestureRecognizer;
+}
+- (UIPanGestureRecognizer *)panGestureRecognizer {
+    if (!_panGestureRecognizer) {
+        _panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self
+                                                                        action:@selector(panHandler:)];;
+        _panGestureRecognizer.delegate = self;
+    }
+    return _panGestureRecognizer;
 }
 
 #pragma mark - Public
@@ -107,6 +129,99 @@ static UILayoutPriority const kMinConstraintPriority = 200;
     
     self.mapView.layer.cornerRadius = 5.0f;
     self.mapView.layer.masksToBounds = YES;
+    
+    [self.chatImageView addGestureRecognizer:self.longPressGestureRecognizer];
+    [self.chatImageView addGestureRecognizer:self.panGestureRecognizer];
+}
+
+#pragma mark - <UIGestureRecognizerDelegate>
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(nonnull UIGestureRecognizer *)otherGestureRecognizer {
+    BOOL result = gestureRecognizer == self.longPressGestureRecognizer && otherGestureRecognizer == self.panGestureRecognizer;
+    return result;
+}
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    BOOL result = (!self.isImageTapped && gestureRecognizer == self.longPressGestureRecognizer)
+               || (self.isImageTapped && gestureRecognizer == self.panGestureRecognizer);
+    return result;
+}
+
+#pragma mark - Private
+
+- (void)animateConstraintsChangesDuration:(CGFloat)duration withAnimation:(void(^)(void))animation withCompletion:(void (^)(BOOL))completion {
+    [UIView animateWithDuration:duration
+                          delay:0.0f
+         usingSpringWithDamping:0.5f
+          initialSpringVelocity:0.5f
+                        options:0
+                     animations:animation
+                     completion:completion];
+}
+- (void)didLongTapOnImage:(UILongPressGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        [self moveImageStartWithCoordinate:[sender locationInView:self.window]];
+    } else if (sender.state == UIGestureRecognizerStateEnded) {
+        [self moveImageBack];
+    }
+}
+- (void)panHandler:(UIPanGestureRecognizer *)sender {
+    if (self.isImageTapped) {
+        CGPoint coordinate = [sender locationInView:self.window];
+        [self updateFrameOfView:self.panningImageView toCoordinate:coordinate];
+    }
+}
+- (void)updateFrameOfView:(UIView *)view withNewFrame:(CGRect)newFrame {
+    //NSLog(@"New frame: %@", NSStringFromCGRect(newFrame));
+    view.frame = newFrame;
+    [self setNeedsDisplay];
+}
+- (void)updateFrameOfView:(UIView *)view toCoordinate:(CGPoint)coordinate {
+    CGSize viewSize = view.frame.size;
+    CGRect newFrame = CGRectMake(coordinate.x - viewSize.width / 2,
+                                 coordinate.y - viewSize.height / 2,
+                                 viewSize.width,
+                                 viewSize.height);
+    [self updateFrameOfView:view withNewFrame:newFrame];
+}
+- (void)growView:(UIView *)view {
+    CGFloat delta = 10.0f;
+    CGRect oldFrame = view.frame;
+    CGRect newFrame = CGRectMake(oldFrame.origin.x - delta,
+                                 oldFrame.origin.y - delta,
+                                 oldFrame.size.width + delta * 2,
+                                 oldFrame.size.height + delta * 2);
+    [self updateFrameOfView:view withNewFrame:newFrame];
+}
+- (void)moveImageStartWithCoordinate:(CGPoint)coordinate {
+    NSLog(@"Start tap");
+    self.isImageTapped = YES;
+
+    self.panningImageView = [[UIImageView alloc] initWithImage:self.chatImageView.image];
+    self.panningImageView.layer.cornerRadius = 5.0f;
+    self.panningImageView.layer.masksToBounds = YES;
+    self.chatImageView.image = [UIImage imageNamed:@"placeholder"];
+    
+    [self updateFrameOfView:self.panningImageView toCoordinate:coordinate];
+    [self.window addSubview:self.panningImageView];
+    
+    __weak __typeof(self) weakSelf = self;
+    [self animateConstraintsChangesDuration:0.5f withAnimation:^{
+        [self growView:weakSelf.panningImageView];
+    }
+                             withCompletion:nil];
+}
+- (void)moveImageBack {
+    NSLog(@"Stop tap");
+    self.isImageTapped = NO;
+    
+    __weak __typeof(self) weakSelf = self;
+    [self animateConstraintsChangesDuration:0.5f withAnimation:^{
+        weakSelf.chatImageView.image = self.panningImageView.image;
+        [weakSelf.panningImageView removeFromSuperview];
+        weakSelf.panningImageView = nil;
+    }
+                             withCompletion:^(BOOL finished) {
+                             }];
 }
 
 @end
